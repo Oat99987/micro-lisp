b483854a723dc77a03026b1fb7555b48faf93a5d
Oat99987 <265971752+Oat99987@users.noreply.github.com>
2026-05-31 12:39:05 +0530

    Revamped clisp.c

--- clisp.c
+++ clisp.c
@@ -4,37 +4,98 @@
 #include <editline/readline.h>
 #include "mpc.h"
 
-static char input[2048];
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
 
-long eval_op(long x, char* op, long y) {
+// Create Enumeration for types
+enum {LVAL_NUM, LVAL_ERR};
 
-    if(strcmp(op, "+") == 0) {return x + y;}
-    if (strcmp(op, "-") == 0 ) {return x - y; }
-    if (strcmp(op, "*") == 0 ) {return x * y ;}
-    if (strcmp(op, "/") == 0 ) {return x / y ;}
-    return 0;
+// Possible error
+enum {LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM};
+
+// New number type: lval
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+// New error type lval
+
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+
+
 }
 
-long eval(mpc_ast_t* t) {
+//Print
 
-    if (strstr(t->tag, "number")){
-        return atoi(t->contents);
+void lval_print(lval v) {
+    switch (v.type) {
+        case LVAL_NUM: printf("%li",v.num); break;
 
+        case LVAL_ERR:
+        if (v.err == LERR_DIV_ZERO) {
+            printf("Error, Division by zero is not mathematically possible");
+
+        }
+        if (v.err == LERR_BAD_OP) {
+            printf("Error, Invalid Operator.");
+        
+        }
+
+        if (v.err == LERR_BAD_NUM) {
+            printf("ERROR: INVALID NUMBER!");
+        }
+        break;
     }
- //Operator is always secondchild
-    char* op = t->children[1]->contents;
-    // Store third child in 'x'
-    long x = eval(t->children[2]);
 
-    // iterate remaining children and combining
+}
+
+void lval_println(lval v) {lval_print(v); putchar('\n'); }
+
+lval eval_op(lval x, char* op, lval y) {
+    if (x.type == LVAL_ERR ) { return x; }
+    if (y.type == LVAL_ERR) { return y; }
+    
+    if(strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+    if (strcmp(op, "-" ) == 0) {return lval_num(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+
+    return y.num == 0
+    ? lval_err(LERR_DIV_ZERO)
+    : lval_num(x.num / y.num);
+
+    return lval_err(LERR_BAD_OP);
+}
+
+
+lval eval(mpc_ast_t* t) {
+
+
+    if (strstr(t->tag, "number")) {
+
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+        
+    }
+    char* op = t->children[1]->contents;
+    lval x = eval(t->children[2]);
+
     int i = 3;
-    while(strstr(t->children[i]->tag, "expr")) {
+    while (strstr(t->children[i]->tag, "expr")){
         x = eval_op(x, op, eval(t->children[i]));
         i++;
     }
-return x;
+    return x;
 }
-
 
 
 int main(int argc, char** argv) {
@@ -53,8 +114,7 @@ int main(int argc, char** argv) {
     ",
        Number, Operator, Expr, Lispy);
 
-    puts("clisp Version 0.0.0.2");
-    puts("This software will soon be released under the terms of the GNU GPL V3 license.");
+    puts("clisp Version 0.0.0.3");
     puts("Press CTRL + C to Exit \n");
 
     // Main part of REPL (Read, Eval, Print, Loop):
@@ -67,8 +127,8 @@ int main(int argc, char** argv) {
 
       mpc_result_t r;
       if (mpc_parse("<stdin>", input, Lispy, &r)){
-        long result = eval(r.output);
-        printf("%li\n", result);
+      lval result = eval(r.output);
+      lval_println(result);
         mpc_ast_delete(r.output);
       } else {
         mpc_err_print(r.error);
